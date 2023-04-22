e72be1b0b95db26c0db3488569e0932ba5d1420f
Tomtide <77882565+Alreaii@users.noreply.github.com>
2023-04-22 10:34:36 +0100

    Update newbanksys.py

--- newbanksys.py
+++ newbanksys.py
@@ -3,36 +3,32 @@ from streamlit_option_menu import option_menu
 from streamlit_extras.switch_page_button import switch_page
 import pandas as pd
 import pnwkit
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 kit = pnwkit.QueryKit("")
 
 allianceId = 3339
-checkterm = ["loan", "repay", "holdings"]
-nid = 107477
-#list of all nations in aa
+checkterm = ["repay", "loan", "holdings", "deposit"]
 
+#api calls
 allnations = kit.query("nations", {"alliance_id": allianceId}, "id nation_name").get()
+allTransactions = kit.query("bankrecs", {"rid": allianceId}, "id receiver_id date note money coal oil uranium iron bauxite gasoline munitions steel aluminum food").get()
+
+#lists & objects
 allnationslist = [i.id for i in allnations.nations]
 allnationsid = [i.nation_name for i in allnations.nations]
 newdf = pd.DataFrame(columns=['nation_name', 'money', 'coal', 'oil', 'uranium', 'iron', 'bauxite', 'gasoline', 'munitions', 'steel', 'aluminum', 'food'])
+transactiondf = pd.DataFrame(columns=['nation_name', 'money', 'coal', 'oil', 'uranium', 'iron', 'bauxite', 'gasoline', 'munitions', 'steel', 'aluminum', 'food'])
+
+
+#functions
+for nation in range(len(allnationslist)):
+    for i in range(len(allnations.nations)):
+        if allnationslist[nation] == allnations.nations[i].id:
+            nation_name = allnations.nations[i].nation_name
+            transactiondf = transactiondf.append({'nation_name': nation_name, 'money': allTransactions.bankrecs[nation].money, 'coal': allTransactions.bankrecs[nation].coal, 'oil': allTransactions.bankrecs[nation].oil, 'uranium': allTransactions.bankrecs[nation].uranium, 'iron': allTransactions.bankrecs[nation].iron, 'bauxite': allTransactions.bankrecs[nation].bauxite, 'gasoline': allTransactions.bankrecs[nation].gasoline, 'munitions': allTransactions.bankrecs[nation].munitions, 'steel': allTransactions.bankrecs[nation].steel, 'aluminum': allTransactions.bankrecs[nation].aluminum, 'food': allTransactions.bankrecs[nation].food}, ignore_index=True)
+
 
 st.title("Alliance Bank")
-st.write("Total Nation Money Holdings")
+st.dataframe(transactiondf)
 
-for nation in range(1): #len(allnationslist)
-    allnationslist[nation] = nid
-    try:
-        nMemberBankTotal = kit.query("bankrecs", {"sid": allnationslist[nation], "rid": allianceId}, "id receiver_id date note money coal oil uranium iron bauxite gasoline munitions steel aluminum food").get()
-        nMemberName = kit.query("nations", {"id": allnationslist[nation]}, "nation_name").get()
-        allwithdrawal = kit.query("bankrecs", {"rid": allnationslist[nation], "sid": allianceId}, "id receiver_id date note money coal oil uranium iron bauxite gasoline munitions steel aluminum food").get()
-        if any(x in nMemberBankTotal.bankrecs[0].note.lower() for x in checkterm):
-            newdf = newdf.append({'nation_name': nMemberName.nations[0].nation_name, 'money': (sum(i.money for i in nMemberBankTotal.bankrecs) - sum(i.money for i in allwithdrawal.bankrecs)), 'coal': nMemberBankTotal.bankrecs[0].coal, 'oil': nMemberBankTotal.bankrecs[0].oil, 'uranium': nMemberBankTotal.bankrecs[0].uranium, 'iron': nMemberBankTotal.bankrecs[0].iron, 'bauxite': nMemberBankTotal.bankrecs[0].bauxite, 'gasoline': nMemberBankTotal.bankrecs[0].gasoline, 'munitions': nMemberBankTotal.bankrecs[0].munitions, 'steel': nMemberBankTotal.bankrecs[0].steel, 'aluminum': nMemberBankTotal.bankrecs[0].aluminum, 'food': nMemberBankTotal.bankrecs[0].food}, ignore_index=True)
-    except IndexError:
-        pass
-
-st.dataframe(newdf)
-
-for i in nMemberBankTotal.bankrecs:
-    st.write(i.money, i.note)
-
-for i in allwithdrawal.bankrecs:
-    st.write(i.money, i.note)
